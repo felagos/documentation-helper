7ab635b5b9c89e1a0a712e524e7a565ff5e680f6
felag <felagossanchez@gmail.com>
2024-04-21 16:58:45 -0400

    Load env variables

--- ingestion.py
+++ ingestion.py
@@ -6,33 +6,48 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Pinecone as PineconeLangChain
 
-INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+separators = ["\n\n", "\n", " ", ""]
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
-pc = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
+def ingest_docs(embeddings, index_name):
+    pc = PineconeVectorStore(index_name=index_name, embedding=embeddings)
 
-load_dotenv()
-
-def ingest_docs():
     loader = ReadTheDocsLoader(
         "langchain-docs/api.python.langchain.com/en/latest/chains"
     )
+    loader.encoding = "latin-1"
 
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600,
+        chunk_overlap=50,
+        separators=separators
+    )
     documents = text_splitter.split_documents(raw_documents)
+
     for doc in documents:
         new_url = doc.metadata["source"]
         new_url = new_url.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
     print(f"Going to add {len(documents)} to Pinecone")
-    PineconeLangChain.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    PineconeLangChain.from_documents(documents, embeddings, index_name=index_name)
     print("****Loading to vectorstore done ***")
 
 
 if __name__ == "__main__":
-    ingest_docs()
+    load_dotenv()
+
+    INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-small",
+        openai_api_key=os.getenv("OPENAI_API_KEY")
+    )
+
+    ingest_docs(
+        embeddings=embeddings,
+        index_name=INDEX_NAME
+    )
